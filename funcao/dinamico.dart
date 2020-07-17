8fc7454a68fd9e1877351398083f85729b945eb7
Marcos Dênis <dee.alves2@gmail.com>
2020-07-17 00:25:02 -0300

    parametros nomeados

--- funcao/dinamico.dart
+++ funcao/dinamico.dart
@@ -1,10 +1,11 @@
 main() {
   juntar(1, 9);
   juntar('Good ', 'evening');
-  juntar('O valor de PI é ', 3.1415);
+  var resultado = juntar('O valor de PI é ', 3.1415);
+  print(resultado.toUpperCase());
 }
 
-dynamic juntar(dynamic a, b) {
+String juntar(dynamic a, b) {
   print(a.toString() + b.toString());
   return a.toString() + b.toString();
 }
---
+++ funcao/opcional.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+main() {
+  int n1 = numeroAleatorio(100);
+  print(n1);
+
+  int n2 = numeroAleatorio();
+  print(n2);
+}
+
+int numeroAleatorio([int maximo = 11]) {
+  return Random().nextInt(maximo);
+}
+
+imprimirData([int dia = 1, int mes = 1, int ano = 1070]) {
+  print('$dia/$mes/$ano');
+}
